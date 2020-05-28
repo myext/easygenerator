134cab84007b695333b00fbc02d6b024b8f27dec
Hleb Zvantsou <myext@yandex.ru>
2020-05-28 19:09:20 +0300

    Update version.php

--- version.php
+++ version.php
@@ -2,6 +2,6 @@
 
 
 $plugin->version   = 2020052800;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2019111200;        // Requires this Moodle version
+$plugin->requires  = 2020052800;        // Requires this Moodle version
 $plugin->component = 'report_easygenerator';  // Full name of the plugin (used for diagnostics)
 
